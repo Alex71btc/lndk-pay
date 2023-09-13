orbitalturtle/lndk

tests: test that ldk node can send onion message

tests/common/mod.rs
@@ -1,13 +1,43 @@
 #![allow(dead_code)]
 
 pub mod ldk_node;
+use crate::common::ldk_node::config::LdkUserInfo;
 
-use bitcoincore_rpc::{bitcoin::Network, json, RpcApi};
+use bitcoin::network::constants::Network;
+use bitcoincore_rpc::{bitcoin::Network as BitcoindNetwork, json, RpcApi};
 use bitcoind::{get_available_port, BitcoinD, Conf};
+use ldk_node::node_api::Node as LdkNode;
 use tempfile::{tempdir, TempDir};
 
-pub async fn setup_test_infrastructure() -> (BitcoinD, TempDir) {
-    setup_bitcoind().await
+pub async fn setup_test_infrastructure(test_name: &str) -> (BitcoinD, TempDir, LdkNode, LdkNode) {
+    let (bitcoind, bitcoind_dir) = setup_bitcoind().await;
+    let connect_params = bitcoind.params.get_cookie_values().unwrap();
+    let ldk1_config = LdkUserInfo {
+        bitcoind_rpc_username: connect_params.0.clone().unwrap(),
+        bitcoind_rpc_password: connect_params.1.clone().unwrap(),
+        bitcoind_rpc_host: String::from("localhost"),
+        bitcoind_rpc_port: bitcoind.params.rpc_socket.port(),
+        ldk_announced_listen_addr: Vec::new(),
+        ldk_peer_listening_port: get_available_port().unwrap(),
+        ldk_announced_node_name: [0; 32],
+        network: Network::Regtest,
+    };
+
+    let ldk2_config = LdkUserInfo {
+        bitcoind_rpc_username: connect_params.0.unwrap(),
+        bitcoind_rpc_password: connect_params.1.unwrap(),
+        bitcoind_rpc_host: String::from("localhost"),
+        bitcoind_rpc_port: bitcoind.params.rpc_socket.port(),
+        ldk_announced_listen_addr: Vec::new(),
+        ldk_peer_listening_port: get_available_port().unwrap(),
+        ldk_announced_node_name: [0; 32],
+        network: Network::Regtest,
+    };
+
+    let ldk1 = ldk_node::start_ldk(ldk1_config, test_name).await;
+    let ldk2 = ldk_node::start_ldk(ldk2_config, test_name).await;
+
+    (bitcoind, bitcoind_dir, ldk1, ldk2)
 }
 
 pub async fn setup_bitcoind() -> (BitcoinD, TempDir) {
@@ -28,7 +58,7 @@ pub async fn setup_bitcoind() -> (BitcoinD, TempDir) {
         .client
         .get_new_address(None, Some(json::AddressType::Bech32))
         .unwrap();
-    let address = address.require_network(Network::Regtest).unwrap();
+    let address = address.require_network(BitcoindNetwork::Regtest).unwrap();
     bitcoind.client.generate_to_address(101, &address).unwrap();
 
     (bitcoind, bitcoind_dir)

Alex71btc/lndk-pay

tests/common/mod.rs
@@ -1,9 +1,39 @@
-use bitcoincore_rpc::{bitcoin::Network, json, RpcApi};
+use bitcoin::network::constants::Network;
+use bitcoincore_rpc::{bitcoin::Network as BitcoindNetwork, json, RpcApi};
 use bitcoind::{get_available_port, BitcoinD, Conf};
+use ldk_sample::config::LdkUserInfo;
+use ldk_sample::node_api::Node as LdkNode;
 use tempfile::{tempdir, TempDir};
 
-pub async fn setup_test_infrastructure() -> (BitcoinD, TempDir) {
-    setup_bitcoind().await
+pub async fn setup_test_infrastructure(test_name: &str) -> (BitcoinD, TempDir, LdkNode, LdkNode) {
+    let (bitcoind, bitcoind_dir) = setup_bitcoind().await;
+    let connect_params = bitcoind.params.get_cookie_values().unwrap();
+    let ldk1_config = LdkUserInfo {
+        bitcoind_rpc_username: connect_params.0.clone().unwrap(),
+        bitcoind_rpc_password: connect_params.1.clone().unwrap(),
+        bitcoind_rpc_host: String::from("localhost"),
+        bitcoind_rpc_port: bitcoind.params.rpc_socket.port(),
+        ldk_announced_listen_addr: Vec::new(),
+        ldk_peer_listening_port: get_available_port().unwrap(),
+        ldk_announced_node_name: [0; 32],
+        network: Network::Regtest,
+    };
+
+    let ldk2_config = LdkUserInfo {
+        bitcoind_rpc_username: connect_params.0.unwrap(),
+        bitcoind_rpc_password: connect_params.1.unwrap(),
+        bitcoind_rpc_host: String::from("localhost"),
+        bitcoind_rpc_port: bitcoind.params.rpc_socket.port(),
+        ldk_announced_listen_addr: Vec::new(),
+        ldk_peer_listening_port: get_available_port().unwrap(),
+        ldk_announced_node_name: [0; 32],
+        network: Network::Regtest,
+    };
+
+    let ldk1 = ldk_sample::start_ldk(ldk1_config, test_name).await;
+    let ldk2 = ldk_sample::start_ldk(ldk2_config, test_name).await;
+
+    (bitcoind, bitcoind_dir, ldk1, ldk2)
 }
 
 pub async fn setup_bitcoind() -> (BitcoinD, TempDir) {
@@ -24,7 +54,7 @@ pub async fn setup_bitcoind() -> (BitcoinD, TempDir) {
         .client
         .get_new_address(None, Some(json::AddressType::Bech32))
         .unwrap();
-    let address = address.require_network(Network::Regtest).unwrap();
+    let address = address.require_network(BitcoindNetwork::Regtest).unwrap();
     bitcoind.client.generate_to_address(101, &address).unwrap();
 
     (bitcoind, bitcoind_dir)