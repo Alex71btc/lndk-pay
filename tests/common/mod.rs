orbitalturtle/lndk

itests: set more granular lnd logs

tests/common/mod.rs
@@ -202,6 +202,7 @@ impl LndNode {
             format!("--tlscertpath={}", cert_path),
             format!("--tlskeypath={}", key_path),
             format!("--logdir={}", log_dir.display()),
+            format!("--debuglevel=debug,PEER=info"),
             format!("--bitcoind.rpcuser={}", connect_params.0.unwrap()),
             format!("--bitcoind.rpcpass={}", connect_params.1.unwrap()),
             format!(

Alex71btc/lndk-pay

tests/common/mod.rs
@@ -206,6 +206,7 @@ impl LndNode {
             format!("--tlscertpath={}", cert_path),
             format!("--tlskeypath={}", key_path),
             format!("--logdir={}", log_dir.display()),
+            format!("--debuglevel=info,PEER=info"),
             format!("--bitcoind.rpcuser={}", connect_params.0.unwrap()),
             format!("--bitcoind.rpcpass={}", connect_params.1.unwrap()),
             format!(