kannapoix/lndk

Make rate limits configureable.
Add 'rate_limit_count' and 'rate_limit_period' to configuration option.

src/lib.rs
@@ -40,6 +40,7 @@ use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config as LogConfig, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
+use rate_limit::RateLimiterCfg;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex, Once};
@@ -129,6 +130,8 @@ pub struct Cfg {
     pub lnd: LndCfg,
     pub signals: LifecycleSignals,
     pub skip_version_check: bool,
+    pub rate_limit_count: u8,
+    pub rate_limit_period: u64,
 }
 
 #[derive(Clone)]
@@ -236,6 +239,10 @@ impl LndkOnionMessenger {
             onion_messenger,
             network,
             args.signals,
+            RateLimiterCfg {
+                call_count: args.rate_limit_count,
+                call_period: Duration::from_secs(args.rate_limit_period),
+            },
         )
         .await
     }

src/onion_messenger.rs
@@ -1,6 +1,6 @@
 use crate::clock::TokioClock;
 use crate::lnd::{features_support_onion_messages, ONION_MESSAGES_OPTIONAL};
-use crate::rate_limit::{RateLimiter, TokenLimiter};
+use crate::rate_limit::{RateLimiter, RateLimiterCfg, TokenLimiter};
 use crate::{LifecycleSignals, LndkOnionMessenger, LDK_LOGGER_NAME};
 use async_trait::async_trait;
 use bitcoin::blockdata::constants::ChainHash;
@@ -45,12 +45,6 @@ const ONION_MESSAGE_TYPE: u32 = 513;
 
 /// MSG_POLL_INTERVAL is the interval at which we poll for outgoing onion messages.
 const MSG_POLL_INTERVAL: Duration = Duration::from_millis(100);
-
-/// DEFAULT_CALL_COUNT is the default number of calls each peer gets per rate limited period.
-const DEFAULT_CALL_COUNT: u8 = 10;
-
-/// DEFAULT_CALL_FREQUENCY is the default period over which peers are rate limited.
-const DEFAULT_CALL_FREQUENCY: Duration = Duration::from_secs(1);
 
 /// Node Id LookUp is a utility struct implementing NodeIdLookUp trait for LDK's OnionMessenger.
 pub struct LndkNodeIdLookUp {
@@ -167,6 +161,7 @@ impl LndkOnionMessenger {
         onion_messenger: OnionMessenger<ES, NS, L, NL, MR, OMH, CMH>,
         network: Network,
         signals: LifecycleSignals,
+        rate_limiter_cfg: RateLimiterCfg,
     ) -> Result<(), ()>
     where
         ES::Target: EntropySource,
@@ -273,8 +268,8 @@ impl LndkOnionMessenger {
         // events we need).
         let rate_limiter = &mut TokenLimiter::new(
             current_peers.keys().copied(),
-            DEFAULT_CALL_COUNT,
-            DEFAULT_CALL_FREQUENCY,
+            rate_limiter_cfg.call_count,
+            rate_limiter_cfg.call_period,
             TokioClock::new(),
         );
         let mut message_sender = CustomMessenger {

src/rate_limit.rs
@@ -50,6 +50,11 @@ pub(crate) struct TokenLimiter<C: Clock> {
     last_update: Instant,
 }
 
+pub(crate) struct RateLimiterCfg {
+    pub(crate) call_count: u8,
+    pub(crate) call_period: Duration,
+}
+
 impl<C: Clock> TokenLimiter<C> {
     /// new creates a TokenLimiter initialized with the clock's current time and loads the set of
     /// peers provided with each allocated call_count hits for the current period.

Alex71btc/lndk-pay

src/lib.rs
@@ -40,6 +40,7 @@ use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config as LogConfig, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
+use rate_limit::RateLimiterCfg;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex, Once};
@@ -129,6 +130,8 @@ pub struct Cfg {
     pub lnd: LndCfg,
     pub signals: LifecycleSignals,
     pub skip_version_check: bool,
+    pub rate_limit_count: u8,
+    pub rate_limit_period_secs: u64,
 }
 
 #[derive(Clone)]
@@ -236,6 +239,10 @@ impl LndkOnionMessenger {
             onion_messenger,
             network,
             args.signals,
+            RateLimiterCfg {
+                call_count: args.rate_limit_count,
+                call_period_secs: Duration::from_secs(args.rate_limit_period_secs),
+            },
         )
         .await
     }

src/onion_messenger.rs
@@ -1,6 +1,6 @@
 use crate::clock::TokioClock;
 use crate::lnd::{features_support_onion_messages, ONION_MESSAGES_OPTIONAL};
-use crate::rate_limit::{RateLimiter, TokenLimiter};
+use crate::rate_limit::{RateLimiter, RateLimiterCfg, TokenLimiter};
 use crate::{LifecycleSignals, LndkOnionMessenger, LDK_LOGGER_NAME};
 use async_trait::async_trait;
 use bitcoin::blockdata::constants::ChainHash;
@@ -45,12 +45,6 @@ const ONION_MESSAGE_TYPE: u32 = 513;
 
 /// MSG_POLL_INTERVAL is the interval at which we poll for outgoing onion messages.
 const MSG_POLL_INTERVAL: Duration = Duration::from_millis(100);
-
-/// DEFAULT_CALL_COUNT is the default number of calls each peer gets per rate limited period.
-const DEFAULT_CALL_COUNT: u8 = 10;
-
-/// DEFAULT_CALL_FREQUENCY is the default period over which peers are rate limited.
-const DEFAULT_CALL_FREQUENCY: Duration = Duration::from_secs(1);
 
 /// Node Id LookUp is a utility struct implementing NodeIdLookUp trait for LDK's OnionMessenger.
 pub struct LndkNodeIdLookUp {
@@ -167,6 +161,7 @@ impl LndkOnionMessenger {
         onion_messenger: OnionMessenger<ES, NS, L, NL, MR, OMH, CMH>,
         network: Network,
         signals: LifecycleSignals,
+        rate_limiter_cfg: RateLimiterCfg,
     ) -> Result<(), ()>
     where
         ES::Target: EntropySource,
@@ -273,8 +268,8 @@ impl LndkOnionMessenger {
         // events we need).
         let rate_limiter = &mut TokenLimiter::new(
             current_peers.keys().copied(),
-            DEFAULT_CALL_COUNT,
-            DEFAULT_CALL_FREQUENCY,
+            rate_limiter_cfg.call_count,
+            rate_limiter_cfg.call_period_secs,
             TokioClock::new(),
         );
         let mut message_sender = CustomMessenger {

src/rate_limit.rs
@@ -50,6 +50,11 @@ pub(crate) struct TokenLimiter<C: Clock> {
     last_update: Instant,
 }
 
+pub(crate) struct RateLimiterCfg {
+    pub(crate) call_count: u8,
+    pub(crate) call_period_secs: Duration,
+}
+
 impl<C: Clock> TokenLimiter<C> {
     /// new creates a TokenLimiter initialized with the clock's current time and loads the set of
     /// peers provided with each allocated call_count hits for the current period.